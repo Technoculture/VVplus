10f7b47cda0fbb14d9414857b075c4eec2d726a3
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-08-26 23:11:43 +0530

    chore: [WIP]change types in tsconfig

--- apps/designer/pages/index.tsx
+++ apps/designer/pages/index.tsx
@@ -2,14 +2,9 @@
 
 export default function Web(): JSX.Element {
   return (
-    
     <div>
       <h1 className="text-3xl font-bold underline">Web</h1>
       {/* <Demo/> */}
     </div>
-    
-    
-  )
-
-  
+  );
 }
