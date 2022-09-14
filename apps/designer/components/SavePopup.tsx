473593631edcc5f5e07132d8a517f5ed996c2b33
Naman Rajput <namanrajput801@gmail.com>
2022-09-15 00:16:09 +0530

    chore: [WIP] make ui from mockup responsive

--- apps/designer/components/SavePopup.tsx
+++ apps/designer/components/SavePopup.tsx
@@ -3,12 +3,7 @@ import React from "react";
 const SavePopup = ({ closeSaveButton }: any) => {
   return (
     <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen backdrop-blur-xl bg-gray">
-      <div
-        className="flex duration-700 bg-white rounded-lg w-72 md:w-96 h-96"
-        style={{
-          transform: "translate(0,0)",
-        }}
-      >
+      <div className="flex duration-700 bg-white rounded-lg w-72 md:w-96 h-96">
         <h1
           className="static pt-2 cursor-pointer md:ml-4/5 "
           onClick={closeSaveButton}
--- apps/designer/components/Welcome.tsx
+++ apps/designer/components/Welcome.tsx
@@ -3,11 +3,22 @@ import React from "react";
 const Welcome = ({ isActive, welcomeClick, toggleButton }: any) => {
   return (
     <div
-      className="z-10 flex items-center justify-center w-64 pb-5 mx-auto duration-500 ease-in-out bg-gray-100 lg:w-96 md:h-1/2 sm:w-96 rounded-xl"
+      className={`
+        z-10 flex items-center justify-center w-64 pb-5 mx-auto duration-500 ease-in-out bg-gray-100 lg:w-96 md:h-1/2 sm:w-96 rounded-xl  
+        ${
+          window.innerWidth < 766 && isActive
+            ? "translate-y-[45vh] translate-x-[-70px] h-48  "
+            : ""
+        } 
+        
+        h-96
+
+        `}
       style={{
-        height: "553px",
+        height: window.innerWidth > 766 ? "73vh" : "",
         marginRight: toggleButton === true ? "32vw" : "",
-        transform: isActive ? "translate(30vw, 0)" : "",
+        transform:
+          isActive && window.innerWidth > 766 ? "translate(30vw, 0)" : "",
         width: toggleButton === true && isActive ? "10px" : "",
         maxHeight: toggleButton === true && isActive ? "113px" : "",
         marginTop: toggleButton === true && isActive ? "20%" : "",
