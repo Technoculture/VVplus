7bbb7839dcc36969f7a6766b18aafa674b4d121e
Naman Rajput <namanrajput801@gmail.com>
2022-09-15 18:34:05 +0530

    refactor: change the name of zustand store

--- apps/designer/components/Layout.tsx
+++ apps/designer/components/Layout.tsx
@@ -4,7 +4,7 @@ import { ScriptProps } from "next/script";
 import LoadingBar from "./elements/LoadingBar";
 import Welcome from "./Welcome";
 import NewDesign from "./NewDesign";
-import useStore from "../globalStore/store";
+import useStore from "../globalStore/navigationStore";
 import SavePopup from "./SavePopup";
 const Layout: React.FC<ScriptProps> = ({ children }) => {
   // TODO: improve this type later
