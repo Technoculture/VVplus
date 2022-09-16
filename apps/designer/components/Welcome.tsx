22dc43481e0230426c0483bf4cce44bad8b0f49f
Naman Rajput <namanrajput801@gmail.com>
2022-09-16 23:59:25 +0530

    chore: [WIP] make ui from mockup responsive

--- apps/designer/components/Welcome.tsx
+++ apps/designer/components/Welcome.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Welcome = ({ isActive, welcomeClick, toggleButton }: any) => {
   return window.innerWidth < 766 && isActive === true ? (
-    <div className=" scroller z-30 flex  overflow-x-scroll  overflow-y-hidden md:hidden whitespace-nowrap px-6  gap-8 mt-[50vh]">
-      <div className="z-20 inline-block h-36  bg-white rounded-lg md:hidden  sm:min-w-[300px] min-w-[250px]"></div>
-      <div className="z-20 inline-block h-36  bg-white rounded-lg md-hidden sm:min-w-[300px] min-w-[250px]"></div>
-      <div className="z-20 inline-block h-36  bg-white rounded-lg md-hidden sm:min-w-[300px] min-w-[250px]"></div>
-      <div className="z-20 inline-block h-36  bg-white rounded-lg md-hidden sm:min-w-[300px] min-w-[250px]"></div>
+    <div className=" scroller z-30 flex  overflow-x-scroll  overflow-y-hidden md:hidden whitespace-nowrap px-6  gap-2 mt-[45vh]">
+      <div className="z-20 inline-block h-44  bg-white rounded-lg md:hidden sm:min-w-[350px] min-w-[270px]"></div>
+      <div className="z-20 inline-block h-44  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
+      <div className="z-20 inline-block h-44  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
+      <div className="z-20 inline-block h-44  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
     </div>
   ) : (
     <div
       className={`
-          z-[100] relative flex items-center justify-center w-64  h-[390px] pb-5 mx-auto sm:mt-0 mt-12 duration-500 ease-in-out bg-gray-100 lg:w-96 md:h-[70vh]  sm:w-80 rounded-xl  
+          z-[100] relative flex items-center justify-center  w-64  h-[390px] pb-5 mx-auto sm:mt-0 mt-12 duration-500 ease-in-out bg-gray-100 lg:w-96 md:h-[70vh]  sm:w-80 rounded-xl  
         
         ${toggleButton === true ? " mr-[32vw] " : ""}
 
@@ -20,7 +20,7 @@ const Welcome = ({ isActive, welcomeClick, toggleButton }: any) => {
         `}
       style={{
         transform:
-          isActive && window.innerWidth > 766 ? "translate(30vw, 0)" : "",
+          isActive && window.innerWidth > 766 ? "translate(28vw, 0)" : "",
         width: toggleButton === true && isActive ? "10px" : "",
         maxHeight: toggleButton === true && isActive ? "113px" : "",
         marginTop: toggleButton === true && isActive ? "20%" : "",
--- apps/designer/components/elements/LoadingBar.tsx
+++ apps/designer/components/elements/LoadingBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingBar = () => {
   return (
-    <div className="flex items-center justify-center mt-[3px] ">
+    <div className="flex  items-center justify-center mt-[3px] ">
       <h1>Loading...</h1>
     </div>
   );
