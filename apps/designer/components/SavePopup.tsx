a41813d9f75acdb832c2634da3bbfe80a3019514
Naman Rajput <namanrajput801@gmail.com>
2022-09-19 20:26:57 +0530

    chore:[WIP] convert ui mockup to function ui

--- apps/designer/components/SavePopup.tsx
+++ apps/designer/components/SavePopup.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-
+import { ImCross } from "react-icons/Im";
 const SavePopup = ({ closeSaveButton }: any) => {
   return (
     <div className="fixed top-0 left-0  flex items-center justify-center w-full h-screen backdrop-blur-3xl z-[100] bg-gray">
       <div className="flex duration-700 bg-white rounded-lg w-72 md:w-96 h-96">
-        <h1
-          className="static pt-px cursor-pointer md:ml-[340px] ml-[250px]"
+        <ImCross
+          className="static pt-px cursor-pointer md:ml-[360px] mt-2 ml-[260px]"
           onClick={closeSaveButton}
-        >
-          click
-        </h1>
+        />
       </div>
     </div>
   );
--- apps/designer/components/Welcome.tsx
+++ apps/designer/components/Welcome.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Welcome = ({ isActive, welcomeClick, toggleButton }: any) => {
   return window.innerWidth < 768 && isActive === true ? (
-    <div className="z-30 flex gap-2 px-6 mt-[50vh] overflow-x-scroll overflow-y-hidden scroller md:hidden whitespace-nowrap">
-      <div className="z-20 inline-block h-44  bg-white rounded-lg md:hidden sm:min-w-[350px] min-w-[270px]"></div>
-      <div className="z-20 inline-block h-44  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
-      <div className="z-20 inline-block h-44  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
-      <div className="z-20 inline-block h-44  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
+    <div className="z-30 flex gap-2 px-6 mt-[47vh] overflow-x-scroll overflow-y-hidden scroller md:hidden whitespace-nowrap">
+      <div className="z-20 inline-block h-[30vh]  bg-white rounded-lg md:hidden sm:min-w-[350px] min-w-[270px]"></div>
+      <div className="z-20 inline-block h-[30vh]  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
+      <div className="z-20 inline-block h-[30vh]  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
+      <div className="z-20 inline-block h-[30vh]  bg-white rounded-lg md-hidden sm:min-w-[350px] min-w-[270px]"></div>
     </div>
   ) : (
     <div
       className={`
-          z-[100] relative flex items-center justify-center  w-64  h-[390px] pb-5 mx-auto  sm:w-[280px] duration-500 ease-in-out  lg:w-96 md:h-[70vh]   rounded-xl  
+          z-[100] relative flex items-center justify-center  w-64  h-[65vh] pb-5 mx-auto  sm:w-[280px] duration-500 ease-in-out  lg:w-96 md:h-[70vh]   rounded-xl  
         
         ${toggleButton === true ? " mr-[32vw] " : ""}
         ${
