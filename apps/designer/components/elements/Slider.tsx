21cd8bdd38929016a1c57c038001d51c2083f466
chandan tiwari <developerchandan1@gmail.com>
2022-10-01 21:56:41 +0530

    fix: fix typescript errors in the frontend

--- apps/designer/components/elements/Slider.tsx
+++ apps/designer/components/elements/Slider.tsx
@@ -8,6 +8,10 @@ import useStore from "../../global-stores/store";
 export default function Slider() {
   const store = useStore();
   const [values, setValues] = React.useState([4]);
+  let dataLen=0;
+  if(data[0]){
+    dataLen=data[0].floors.length -1;
+  }
   // console.log(store.floor);
   return (
     <div className="w-full flex justify-center">
@@ -16,11 +20,11 @@ export default function Slider() {
         <Range
           step={1}
           min={0}
-          max={data[0].floors.length - 1}
+          max={dataLen}
           values={values}
           onChange={(values) => {
             setValues(values);
-            store.changeFloor(values[0]);
+            store.changeFloor(values[0] || 0);
           }}
           renderTrack={({ props, children }) => (
             <div
@@ -39,7 +43,7 @@ export default function Slider() {
         />
         <span>Floor: {values[0]}</span>
         <br />
-        <span>Set on 0 for plot and {data[0].floors.length - 1} for roof</span>
+        <span>Set on 0 for plot and {dataLen} for roof</span>
         <br />
         <br />
       </div>
--- apps/designer/pages/_app.tsx
+++ apps/designer/pages/_app.tsx
@@ -1,19 +1,28 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { AppContext, AppInitialProps, AppLayoutProps } from "next/app";
-import type { NextComponentType } from "next";
+//import { AppContext, AppInitialProps, AppLayoutProps } from "next/app";
+//import type { NextComponentType } from "next";
 
-const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
-  Component,
-  pageProps,
-}: AppLayoutProps) => {
+// const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
+//   Component,
+//   pageProps,
+// }: AppLayoutProps) => {
+//   return (
+//     <>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </>
+//   );
+// };
+import { AppProps } from "next/app";
+// export default MyApp;
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Layout>
-        <Component {...pageProps} />
+          <Component {...pageProps} />
       </Layout>
     </>
   );
-};
-
-export default MyApp;
+}
