4e4bf7e00028e0159e6adae86db5d90deb4b3c87
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-11-09 21:05:05 +0530

    chore: add theme to tailwind config

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,10 +1,37 @@
 module.exports = {
-    content: [
-        '../../packages/ui/components/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}'
-    ],
-    theme: {
-        extend: {}
+  content: [
+    "../../packages/ui/components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      RobotoSerif: ["Roboto Serif", "serif"],
     },
-    plugins: []
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        "fade-out": {
+          from: {
+            opacity: "1",
+          },
+          to: {
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s ease-out",
+        "fade-out": "fade-out 0.5s ease-out",
+      },
+    },
+  },
+  plugins: [],
 };
