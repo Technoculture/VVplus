10c300fb6bbbc97bb07606cb48abd0b606c5fa4a
chandan tiwari <developerchandan1@gmail.com>
2022-11-30 20:12:40 +0530

    feat(dataman):add dataman to the vvplus repo

---
+++ apps/Dataman/src/screens/home.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hello this one is the home screen for the dataman</Text>
+    </View>
+  );
+};
