e94e70af0a424b030c758c537f77d1f4e1d271bb
cr1spyvada <vaisakhnp0700@gmail.com>
2022-12-13 11:44:43 +0530

    feat: notification screen

--- apps/Dataman/src/screens/notifications.tsx
+++ apps/Dataman/src/screens/notifications.tsx
@@ -1,9 +1,36 @@
+import { List } from "@ant-design/react-native";
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
 export const NotificationScreen = () => {
   return (
-    <View>
-      <Text>Hello this one is the notification screen for the dataman</Text>
+    <View
+      style={{
+        flexDirection: "column",
+      }}
+    >
+      <Text style={{ fontSize: 28, fontWeight: "600", paddingHorizontal: 10 }}>
+        Notifications
+      </Text>
+      <View>
+        <List>
+          {["Notification1", "Notification2"].map((item, idx) => (
+            <List.Item
+              key={idx}
+              style={styles.container}
+              extra={<AntDesign name="right" size={20} color="#969696" />}
+            >
+              {item}
+            </List.Item>
+          ))}
+        </List>
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    borderColor: "#969696",
+  },
+});
