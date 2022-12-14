c515a9b73c6a6ba27f63d0bd193512b0fecfeec2
sameera222 <100690618+sameera222@users.noreply.github.com>
2022-12-14 09:54:22 +0530

    chore: setup ant design for dependencies

--- apps/Dataman/src/screens/home.tsx
+++ apps/Dataman/src/screens/home.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import { Button } from "@ant-design/react-native";
+
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>Hello this one is the home screen for the dataman</Text>
+      <Text>Hello this one is the home screen for the dataman App</Text>
+      <Button type="primary">Click Me!</Button>
     </View>
   );
 };
