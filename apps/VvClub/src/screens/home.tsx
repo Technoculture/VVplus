305f097672b62be479309e16ccd0647b4d0a7a28
chandan tiwari <developerchandan1@gmail.com>
2022-12-19 03:24:40 +0530

    chore:add slug and project id to it

--- apps/VvClub/src/screens/home.tsx
+++ apps/VvClub/src/screens/home.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text,SafeAreaView} from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export const HomeScreen = () => {
