fcf25236cb809dfea6b735a051c4721fc5248666
sameera222 <100690618+sameera222@users.noreply.github.com>
2023-01-24 19:12:44 +0530

    chore: add await response to the location

--- apps/Dataman/src/screens/Location.tsx
+++ apps/Dataman/src/screens/Location.tsx
@@ -3,31 +3,58 @@ import { Text, View } from "react-native";
 import * as Location from "expo-location";
 
 export const LocationScreen = () => {
-  const [location, setLocation] = useState({});
   const [errorMsg, setErrorMsg] = useState("");
+  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+    "Wait, we are fetching you location..."
+  );
 
   useEffect(() => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-      const location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+    CheckIfLocationEnabled();
+    GetCurrentLocation();
   }, []);
 
+  const CheckIfLocationEnabled = async () => {
+    const enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled) {
+      setErrorMsg("Location Service not enabled");
+    } else {
+      setLocationServiceEnabled(enabled);
+    }
+  };
+
+  const GetCurrentLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      setErrorMsg("Permission not granted");
+    }
+
+    const { coords } = await Location.getCurrentPositionAsync();
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+      const response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      for (const item of response) {
+        const address = `${item.name}, ${item.country},${item.district},${item.subregion}, ${item.region},${item.postalCode}, ${item.city}`;
+
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
   let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
   }
 
   return (
     <View>
-      <Text> {text}</Text>
+      <Text>{displayCurrentAddress}</Text>
     </View>
   );
 };
