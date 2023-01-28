3fd4dc6608bc1e9e6c87a2a21c24bead5595d6e6
cr1spyvada <vaisakhnp0700@gmail.com>
2023-01-28 18:50:45 +0530

    chore: removed babylon dependencies

--- apps/designer/src/components/elements/Model.tsx
+++ apps/designer/src/components/elements/Model.tsx
@@ -1,16 +1,19 @@
 import data from "../../../public/house.json";
-import { Scene } from "three";
+import { Group, Scene } from "three";
 import { useGLTF } from "@react-three/drei";
 
 export function createModel(scene: Scene) {
   data.floors.map((element) => {
+    const floor = new Group();
     element.floorStructure.map((e) => {
       const model = useGLTF(data.baseUrl + e.file);
-      model.scene.name = element.floorName + "-" + e.file.split(".")[0];
+      model.scene.name = element.id + "-" + e.file.split(".")[0];
       model.scene.scale.set(10, 10, 10);
       model.scene.position.set(0, 0, 0);
-      scene.add(model.scene);
+      floor.add(model.scene);
       useGLTF.preload(data.baseUrl + e.file);
+      floor.name = element.id;
     });
+    scene.add(floor);
   });
 }
